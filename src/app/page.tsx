fb18a904c56472ae44dff976b30a455c843e8ffd
nmaj060201152 <https://github.com/nmaj060201152>
2025-01-03 20:21:05 +0500

    test 3rd Jan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className='bg-slate-500 h-[80px] w-[900px]'>
-        <h1>Navaid Majeed Sumalani</h1>
-        <h1>Junaid Majeed Sumalani</h1>
-
+      <div className='flex bg-slate-500 h-[80px] w-screen justify-between m-0 p-0'>
+        <div className='flex bg-slate-500 h-[80px] w-screen justify-between m-0 p-0'>Nv 03-01-2025</div>
+        <div></div>
       </div>
     </div>
   )
